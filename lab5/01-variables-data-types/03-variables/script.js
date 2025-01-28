87d7e72642fc27a7ba3e7538c3dd08bb2b5fb723
O0MlM <oomim.sarisa@gmail.com>
2025-01-28 12:05:32 +0700

    lab5

---
+++ lab5/01-variables-data-types/03-variables/script.js
@@ -0,0 +1,25 @@
+let name;
+let surname;
+
+let surName;
+
+let cats = ['Cherry','Joss'];
+
+let schoolname;
+let school_name;
+
+let mySchoolName;
+let my_school_name;
+
+
+
+let a=1;
+let b=2;
+let c=3;
+
+console.log(a, typeof a)
+console.log(b, typeof b)
+console.log(c, typeof c)
+
+let bigNumber;
+console.log(bigNumber, typeof bigNumber);
